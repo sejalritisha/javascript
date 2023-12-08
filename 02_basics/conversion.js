acc938de61fc1963e1408362fd2e81ce1c2cba02
Sejal Ritisha <125900614+sejalritisha@users.noreply.github.com>
2023-12-08 18:12:35 +0000

    coversion of datatypes

---
+++ 02_basics/conversion.js
@@ -0,0 +1,13 @@
+// var num=123;
+// console.log(String(num));
+// console.log(Boolean(num)); 1-true 0-false
+// var name="sej54"
+// console.log(Number(name));//nan
+// console.log(String(num));
+// console.log(Boolean(name)); //""-false "dgd"-true
+// var islog=true;
+// console.log(String(islog));//true
+// console.log(Number(islog));//1
+var islog=false;
+console.log(String(islog));//false
+console.log(Number(islog));//0
