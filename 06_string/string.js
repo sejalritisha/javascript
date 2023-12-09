e8760841f517be67c11f37f9c75176ec7a8cf99c
Sejal Ritisha <125900614+sejalritisha@users.noreply.github.com>
2023-12-09 13:59:24 +0000

    learning basics

---
+++ 06_string/string.js
@@ -0,0 +1,9 @@
+var name="sejal ritisha"
+// var age=23;
+// var college="jnu"
+// var course="mca"
+// console.log(`hello my name is ${name} and my age is ${age}`)
+// var arr=name.split(' ');
+// console.log(arr[0])
+// console.log(name.substring(-2,8))
+console.log(name.slice(-1,-6))
---
+++ 08_date/date.js
@@ -0,0 +1,10 @@
+// let myDate=new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+let mycurrent=new Date(2023,0,1)
+console.log(mycurrent)
+let time=Date.now();
+console.log(time)
+console.log(mycurrent.getTime())
